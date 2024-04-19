bc34b00a1952f5b2244fee53bde4377f28e4d7bd
Hung Nguyen <nguyenhungg127@gmail.com>
2024-04-19 10:37:16 +0700

    fix there was an error running the selected code generator Package restore failed

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac4062452154fdd5cc300793e782d1468fe3bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd310f231b2bc67fd4b5a854d3adc94a3a1ee95")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
