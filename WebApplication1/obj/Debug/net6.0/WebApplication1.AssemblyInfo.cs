fce8ff01d69d661def3b61da228f0a4eff287911
Hung Nguyen <nguyenhungg127@gmail.com>
2024-04-19 10:40:01 +0700

    add crud categories

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc34b00a1952f5b2244fee53bde4377f28e4d7bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e292b302d6daf7db8f37c92c1a89daffdaff3080")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
