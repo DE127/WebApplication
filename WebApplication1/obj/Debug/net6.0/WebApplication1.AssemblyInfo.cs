1c240e14a557bf3f87e4182a57af8c4b02f650b6
Hung Nguyen <nguyenhungg127@gmail.com>
2024-04-19 10:40:49 +0700

    add crud product

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e292b302d6daf7db8f37c92c1a89daffdaff3080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce8ff01d69d661def3b61da228f0a4eff287911")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
